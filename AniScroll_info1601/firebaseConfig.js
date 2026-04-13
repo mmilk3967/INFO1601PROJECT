f971bb440a2459668427ec6a52fefe0c134258d8
mmilk3967 <mmilk3967@gmail.com>
2026-04-12 21:13:40 -0400

    Add files via upload

---
+++ AniScroll_info1601/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6hc8jjaOQadLsno-75eLSINQ9gPZ0fOU",
+  authDomain: "aniscroll-23cd8.firebaseapp.com",
+  projectId: "aniscroll-23cd8",
+  storageBucket: "aniscroll-23cd8.firebasestorage.app",
+  messagingSenderId: "442473446021",
+  appId: "1:442473446021:web:3d0ee6d7bca8ae689e87ec",
+  measurementId: "G-ZKTDDL1TG1"
+};
+
+export default firebaseConfig;
